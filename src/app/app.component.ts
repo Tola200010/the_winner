803abd5a1fd71ec185c0e6a3edeeb1e91ffb231c
chhittola <tola.cambodia000@gmail.com>
2024-05-25 11:35:44 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { RandomService } from './services/random.service';
+import { CommonModule } from '@angular/common';
+import confetti from 'canvas-confetti';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'the-winner';
+  randomService = inject(RandomService)
+  randomNumber1: number = 0;
+  randomNumber2: number = 0;
+  randomNumber3: number = 0;
+  intervalId: any;
+  isGenerating: boolean = false;
+  startRandomNumberGeneration() {
+    this.isGenerating = !this.isGenerating;
+    if(this.isGenerating){
+      this.intervalId = setInterval(() => {
+        this.randomNumber1 = this.randomService.getRandomNumber(1, 9);
+        this.randomNumber2 = this.randomService.getRandomNumber(1, 9);
+        this.randomNumber3 = this.randomService.getRandomNumber(1, 9);
+      }, 100); // Generates a new random number every 100ms
+    }else{
+      this.isGenerating = false;
+      if (this.intervalId) {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+        this.celebrate();
+      }
+    }
+  }
+  stopRandomNumberGeneration() {
+    this.isGenerating = false;
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  }
+  celebrate() {
+    const duration = 3000; // in milliseconds
+  
+    confetti({
+      particleCount: 100,
+      spread: 160,
+      origin: { y: 0.6 },
+    });
+  
+    // Clear confetti after a certain duration
+    setTimeout(() => confetti.reset(), duration);
+  }
 }
---
+++ src/app/services/random.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RandomService {
+
+  constructor() { }
+  getRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
